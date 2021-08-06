d7b47fb109989748c685201e5440b127cdefa723
Greet Van der Linden <vanderlindengreet@gmail.com>
2021-08-06 18:00:23 +0200

    desktop version theme 1 done

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,2 @@
 import "../css/style.scss";
-import Square from "./classes/Square";
-
-new Square(20, 20, 200, document.querySelector(".test"));
-console.log(process.env.SECRET);
+// import Square from "./classes/Square";
